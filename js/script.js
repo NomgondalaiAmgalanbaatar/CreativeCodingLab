042eda2521f64889cf80b27b3a74f86f677e9482
Nomgo <amgaanomgoo@gmail.com>
2024-10-18 17:12:07 +0800

    new

---
+++ js/script.js
@@ -0,0 +1,15 @@
+alert("aah you pressed me!");
+console.log("yoo he opened");
+
+function hoverText() {
+    alert("BOOM!");
+}
+
+function setup() {
+    let canvas = createCanvas(500, 400);
+    canvas.parent("p5-canvas-container")
+}
+
+function draw() {
+    background(220);
+}
